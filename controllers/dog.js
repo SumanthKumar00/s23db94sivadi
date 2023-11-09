460fb11f807231f8e6be12291cac524d76c98bff
Sumanth Kumar Sivadi <sivadisumanth@gmail.com>
2023-11-09 04:04:38 -0600

    Update dog.js

--- controllers/dog.js
+++ controllers/dog.js
@@ -45,4 +45,25 @@ exports.dog_view_all_Page = async function(req, res) {
     }
     };
     
+/// Handle Costume create on POST.
+exports.dog_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new dog();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.dog_name = req.body.dog_name;
+    document.dog_color = req.body.dog_color;
+    document.dog_age = req.body.dog_age;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
     

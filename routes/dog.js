8ed3e1652658748facf53a1b6d2c8d5b5d5b4f30
Sumanth Kumar Sivadi <sivadisumanth@gmail.com>
2023-11-10 02:07:01 -0600

    ids

--- routes/dog.js
+++ routes/dog.js
@@ -1,6 +1,9 @@
 var express = require('express');
 const dog_controlers= require('../controllers/dog');
 var router = express.Router();
-/* GET costumes */
+/* GET dogs */
 router.get('/', dog_controlers.dog_view_all_Page );
+//GET request for one dog.
+router.get('/dogs/:id', dog_controlers.dog_detail);
+
 module.exports = router;
